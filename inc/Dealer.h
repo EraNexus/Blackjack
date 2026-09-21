6066e3216608976b74ff8a5ccd383d8b3f103311
Evan Rutledge <emrut2004@gmail.com>
2026-09-21 11:34:19 -0400

    Version 1.0 completed. More updates to come!

--- inc/Dealer.h
+++ inc/Dealer.h
@@ -8,7 +8,7 @@
 #include <unordered_map>
 #include <iostream>
 
-class Dealer{
+class Dealer {
     public:
         std::vector<std::string> initialDeal(std::vector<std::string>& hand, std::vector<std::string>& deck);
         std::vector<std::string> hit(std::vector<std::string>& hand, std::vector<std::string>& deck);
